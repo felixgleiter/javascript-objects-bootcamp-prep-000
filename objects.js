b93854f938357feab4840a6b36b0b58a94e20edf
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 11:09:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {"GZA" : "4th Chamber"};
 
-function updatePlaylist(playlist, artist, song) {
-  var newPlaylist = Object.assign(playlist, {artist : song});
+function updatePlaylist(playlist, artistName, songTitle) {
+  var newPlaylist = Object.assign(playlist, {artistName : songTitle});
   return newPlaylist;
 }
