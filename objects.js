bfa33dfb8aa718cf0e88db1fdac2290ba2cdf5e3
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 11:15:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {"GZA" : "4th Chamber"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist.[artistName] = songTitle;
   return playlist;
 }
